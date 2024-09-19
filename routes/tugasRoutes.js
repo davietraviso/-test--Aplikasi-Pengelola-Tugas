35bf02a29d7a2f0e0fbb0fd8d73fca73b967c112
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-19 16:42:48 +0700

    tugas endpoind

---
+++ routes/tugasRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const route = express.Router()
+
+route.get('/', async (req, res) => {
+  res.status(200).json({
+    metadata: "tugas enpoind"
+  })
+})
+
+module.exports = route
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const port = 3200
 
+const tugasEnpoint = require('./routes/tugasRoutes')
+
 const app = express()
+
+app.use('/tugas', tugasEnpoint)
+
+
 
 app.listen(port, () => console.log(`port running to ${port}`))
