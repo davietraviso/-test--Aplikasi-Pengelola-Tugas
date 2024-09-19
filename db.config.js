0857781f827177baf93e25a86ab6e6a59c9f65df
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-19 23:32:18 +0700

    data base berhasil

--- db.config.js
+++ db.config.js
@@ -11,3 +11,5 @@ const sequelize = new Sequelize(
     dialect: process.env.DB_DIALECT
   }
 )
+
+module.exports = sequelize
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
+const cors = require('cors')
 const port = 3200
+
+const sequelize = require('./db.config')
+sequelize.sync().then(() => console.log("database ready"))
 
 const tugasEnpoint = require('./routes/tugasRoutes')
 
 const app = express()
+app.use(cors())
 
 app.use('/tasks', tugasEnpoint)
-
 
 
 app.listen(port, () => console.log(`port running to ${port}`))
