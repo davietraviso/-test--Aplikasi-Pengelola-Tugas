e32e9c68e1f39f4554c40259a3590d679eee9ad8
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-19 23:10:28 +0700

    menambakan .env

--- db.config.js
+++ db.config.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('', '', '', {
-  host: '',
-  dialect: ''
-})
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  }
+)
